d517945866d03cc34f121c6c8e4d07fecdb2c4a5
Ainur <kildibekov.ainur@mail.ru>
2022-09-01 10:20:10 +0500

    Включил теги структур для переменных среды

--- homework_8/config/config.go
+++ homework_8/config/config.go
@@ -8,26 +8,13 @@ import (
 )
 
 type Config struct {
-	Port        uint64
-	DbUrl       string
-	JaegerUrl   string
-	SentryUrl   string
-	KafkaBroker string
-	SomeAppId   string
-	SomeAppKey  string
-}
-
-type envConfig struct {
-	DB_URL       string
-	JAEGER_URL   string
-	SENTRY_URL   string
-	KAFKA_BROKER string
-}
-
-type flagConfig struct {
-	Port       uint64
-	SomeAppId  string
-	SomeAppKey string
+	Port        uint64 `ignored:"true"`
+	DbUrl       string `required:"true" envconfig:"DB_URL"`
+	JaegerUrl   string `required:"true" envconfig:"JAEGER_URL"`
+	SentryUrl   string `required:"true" envconfig:"SENTRY_URL"`
+	KafkaBroker string `default:"kafka:9092" envconfig:"KAFKA_BROKER"`
+	SomeAppId   string `ignored:"true"`
+	SomeAppKey  string `ignored:"true"`
 }
 
 type ConfUrl struct {
@@ -44,28 +31,22 @@ func (u ConfUrl) decode() (string, error) {
 }
 
 func Read() (Config, error) {
-	flagConfigData, err := readFlags()
+	configData := Config{}
+
+	err := readFlags(&configData)
 	if err != nil {
 		return Config{}, err
 	}
-	envConfigData, err := readEnv()
+	err = readEnv(&configData)
 	if err != nil {
 		return Config{}, err
 	}
 
-	return Config{
-		Port:        flagConfigData.Port,
-		DbUrl:       envConfigData.DB_URL,
-		JaegerUrl:   envConfigData.JAEGER_URL,
-		SentryUrl:   envConfigData.SENTRY_URL,
-		KafkaBroker: envConfigData.KAFKA_BROKER,
-		SomeAppId:   flagConfigData.SomeAppId,
-		SomeAppKey:  flagConfigData.SomeAppKey,
-	}, nil
+	return configData, nil
 }
 
 // Чтение флагов
-func readFlags() (flagConfig, error) {
+func readFlags(confData *Config) error {
 	fPort := flag.Uint64("port", 8080, "Port number")
 	fSomeAppId := flag.String("some_app_id", "", "Some App ID")
 	fSomeAppKey := flag.String("some_app_key", "", "Some App Key")
@@ -73,49 +54,43 @@ func readFlags() (flagConfig, error) {
 	flag.Parse()
 
 	if *fPort == 0 {
-		return flagConfig{}, errors.New("port can't be empty")
+		return errors.New("port can't be empty")
 	}
 	if *fSomeAppId == "" {
-		return flagConfig{}, errors.New("some_app_id can't be empty")
+		return errors.New("some_app_id can't be empty")
 	}
 	if *fSomeAppKey == "" {
-		return flagConfig{}, errors.New("some_app_key can't be empty")
+		return errors.New("some_app_key can't be empty")
 	}
 
-	return flagConfig{
-		Port:       *fPort,
-		SomeAppId:  *fSomeAppId,
-		SomeAppKey: *fSomeAppKey,
-	}, nil
+	confData.Port = *fPort
+	confData.SomeAppId = *fSomeAppId
+	confData.SomeAppKey = *fSomeAppKey
+
+	return nil
 }
 
 // Чтение переменных окружения
-func readEnv() (envConfig, error) {
-	envConfigData := envConfig{}
-	err := envconfig.Process("", &envConfigData)
+func readEnv(confData *Config) error {
+	err := envconfig.Process("", confData)
 	if err != nil {
-		return envConfig{}, err
+		return err
 	}
 
-	_, err = ConfUrl{url: envConfigData.DB_URL}.decode()
+	_, err = ConfUrl{url: confData.DbUrl}.decode()
 	if err != nil {
-		return envConfig{}, errors.New("DB_URL is incorrect")
+		return errors.New("DB_URL is incorrect")
 	}
 
-	_, err = ConfUrl{url: envConfigData.JAEGER_URL}.decode()
+	_, err = ConfUrl{url: confData.JaegerUrl}.decode()
 	if err != nil {
-		return envConfig{}, errors.New("JAEGER_URL is incorrect")
+		return errors.New("JAEGER_URL is incorrect")
 	}
 
-	_, err = ConfUrl{url: envConfigData.SENTRY_URL}.decode()
+	_, err = ConfUrl{url: confData.SentryUrl}.decode()
 	if err != nil {
-		return envConfig{}, errors.New("SENTRY_URL is incorrect")
+		return errors.New("SENTRY_URL is incorrect")
 	}
 
-	return envConfig{
-		DB_URL:       envConfigData.DB_URL,
-		JAEGER_URL:   envConfigData.JAEGER_URL,
-		SENTRY_URL:   envConfigData.SENTRY_URL,
-		KAFKA_BROKER: envConfigData.DB_URL,
-	}, nil
+	return nil
 }
